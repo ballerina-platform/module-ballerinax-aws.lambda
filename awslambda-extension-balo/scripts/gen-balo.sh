71b27f456f39a93ea6ab6fd817ac90d244cba8fd
Tharik Kanaka <tharik.kanaka@gmail.com>
2019-11-02 00:20:34 +0530

    Update build command

--- awslambda-extension-balo/scripts/gen-balo.sh
+++ awslambda-extension-balo/scripts/gen-balo.sh
@@ -33,11 +33,11 @@ rm -rf ${AWSLAMBDA_BALLERINA_PROJECT}/target
 if ! hash pushd 2>/dev/null
 then
     cd ${AWSLAMBDA_BALLERINA_PROJECT}
-    ${EXECUTABLE} compile
+    ${EXECUTABLE} build -a
     cd -
 else
     pushd ${AWSLAMBDA_BALLERINA_PROJECT} /dev/null 2>&1
-        ${EXECUTABLE} compile
+        ${EXECUTABLE} build -a
     popd > /dev/null 2>&1
 fi
 
