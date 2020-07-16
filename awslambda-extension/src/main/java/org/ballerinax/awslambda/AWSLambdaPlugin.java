27f1c6f6d46149152ba8c8358f41bdc3d2bd792b
Hemika Kodikara <hemika.kodikara@gmail.com>
2020-07-16 22:47:38 +0530

    remove printing update-function-configuration cmd

--- awslambda-extension/src/main/java/org/ballerinax/awslambda/AWSLambdaPlugin.java
+++ awslambda-extension/src/main/java/org/ballerinax/awslambda/AWSLambdaPlugin.java
@@ -329,9 +329,8 @@ public class AWSLambdaPlugin extends AbstractCompilerPlugin {
         OUT.println("\n\tRun the following commands to deploy each Ballerina AWS Lambda function:");
         OUT.println("\taws lambda create-function --function-name <FUNCTION_NAME> --zip-file fileb://"
                 + LAMBDA_OUTPUT_ZIP_FILENAME + " --handler " + balxName
-                + ".<FUNCTION_NAME> --runtime provided --role <LAMBDA_ROLE_ARN> --timeout 10 --memory-size 1024");
-        OUT.println("\taws lambda update-function-configuration --function-name <FUNCTION_NAME> "
-                + "--layers arn:aws:lambda:<REGION_ID>:141896495686:layer:ballerina:2");
+                + ".<FUNCTION_NAME> --runtime provided --role <LAMBDA_ROLE_ARN> --layers "
+                + "arn:aws:lambda:<REGION_ID>:141896495686:layer:ballerina:2");
         OUT.println("\n\tRun the following command to re-deploy an updated Ballerina AWS Lambda function:");
         OUT.println("\taws lambda update-function-code --function-name <FUNCTION_NAME> --zip-file fileb://"
                 + LAMBDA_OUTPUT_ZIP_FILENAME);
--- samples/sample1/README.md
+++ samples/sample1/README.md
@@ -38,8 +38,7 @@ Generating executable
         The Ballerina AWS Lambda layer information can be found at https://ballerina.io/deployment/aws-lambda.
 
 	Run the following commands to deploy each Ballerina AWS Lambda function:
-	aws lambda create-function --function-name <FUNCTION_NAME> --zip-file fileb://aws-ballerina-lambda-functions.zip --handler functions.<FUNCTION_NAME> --runtime provided --role <LAMBDA_ROLE_ARN> --timeout 10 --memory-size 1024
-	aws lambda update-function-configuration --function-name <FUNCTION_NAME> --layers <BALLERINA_LAYER_ARN>
+	aws lambda create-function --function-name <FUNCTION_NAME> --zip-file fileb://aws-ballerina-lambda-functions.zip --handler functions.<FUNCTION_NAME> --runtime provided --role <LAMBDA_ROLE_ARN> --layers <BALLERINA_LAYER_ARN>
 
 	Run the following command to re-deploy an updated Ballerina AWS Lambda function:
 	aws lambda update-function-code --function-name <FUNCTION_NAME> --zip-file fileb://aws-ballerina-lambda-functions.zip
